339b5719837a6e1990537fcbddced8ab7d5c3e7a
Mayel de Borniol <git@mayel.space>
2025-03-31 18:22:02 +0100

    f

--- test/smart_input/create_message_test.exs
+++ test/smart_input/create_message_test.exs
@@ -18,7 +18,11 @@ defmodule Bonfire.UI.Messages.CreateMessageTest do
     {:ok, conn: conn, account: account, me: me, recipient: recipient}
   end
 
-  test "message shows up in sender's message threads list", %{conn: conn, me: me, recipient: recipient} do
+  test "message shows up in sender's message threads list", %{
+    conn: conn,
+    me: me,
+    recipient: recipient
+  } do
     content = "here is an epic html message"
 
     attrs = %{
@@ -34,7 +38,11 @@ defmodule Bonfire.UI.Messages.CreateMessageTest do
     |> assert_has("#message_threads", text: content)
   end
 
-  test "message shows up in recipient's message threads list", %{conn: conn, me: me, recipient: recipient} do
+  test "message shows up in recipient's message threads list", %{
+    conn: conn,
+    me: me,
+    recipient: recipient
+  } do
     content = "here is an epic html message"
 
     attrs = %{
@@ -54,49 +62,58 @@ defmodule Bonfire.UI.Messages.CreateMessageTest do
   end
 
   test "does not show up on my profile timeline", %{conn: conn, me: me, recipient: recipient} do
-      content = "here is an epic html message"
-      next = "/settings"
-      # |> IO.inspect
-      {:ok, view, _html} = live(conn, next)
-      # open_browser(view)
-      live_async_wait(view)
+    content = "here is an epic html message"
+    next = "/settings"
+    # |> IO.inspect
+    {:ok, view, _html} = live(conn, next)
+    # open_browser(view)
+    live_async_wait(view)
 
-      assert sent =
-               view
-               |> form("#smart_input form")
-               |> render_submit(%{
-                 "create_object_type" => "message",
-                 "to_circles" => [id(recipient)],
-                 "post" => %{"post_content" => %{"html_body" => content}}
-               })
+    assert sent =
+             view
+             |> form("#smart_input form")
+             |> render_submit(%{
+               "create_object_type" => "message",
+               "to_circles" => [id(recipient)],
+               "post" => %{"post_content" => %{"html_body" => content}}
+             })
 
-      next = "/user"
-      # |> IO.inspect
-      {:ok, profile, _html} = live(conn, next)
-      refute has_element?(profile, "[data-id=feed]", content)
+    next = "/user"
+    # |> IO.inspect
+    {:ok, profile, _html} = live(conn, next)
+    refute has_element?(profile, "[data-id=feed]", content)
   end
 
-  test "recipient can send reply, which appears instantly on thread page", %{me: me, recipient: recipient, conn: conn} do
-      attrs = %{
-        post_content: %{
-          summary: "summary",
-          name: "test message name",
-          html_body: "first message"
-        }
+  test "recipient can send reply, which appears instantly on thread page", %{
+    me: me,
+    recipient: recipient,
+    conn: conn
+  } do
+    attrs = %{
+      post_content: %{
+        summary: "summary",
+        name: "test message name",
+        html_body: "first message"
       }
-      assert {:ok, op} =
-               Messages.send(me, attrs, recipient)
-      content = "epic reply"
-      reply = %{
-        post_content: %{ html_body: content},
-        reply_to_id: op.id
-      }
-      next = "/messages/#{id(op)}"
-      # |> IO.inspect
-      assert {:ok, re} =
-        Messages.send(me, reply, recipient)
-        conn
-        |> visit(next)
-        |> assert_has("article", text: content)
+    }
+
+    assert {:ok, op} =
+             Messages.send(me, attrs, recipient)
+
+    content = "epic reply"
+
+    reply = %{
+      post_content: %{html_body: content},
+      reply_to_id: op.id
+    }
+
+    next = "/messages/#{id(op)}"
+    # |> IO.inspect
+    assert {:ok, re} =
+             Messages.send(me, reply, recipient)
+
+    conn
+    |> visit(next)
+    |> assert_has("article", text: content)
   end
 end
