f9d550b3151470c330b97efa3a2d6f6c63761b03
Mayel de Borniol <git@mayel.space>
2025-04-09 20:48:14 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/868

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -23,6 +23,8 @@ defmodule Bonfire.UI.Messages.DataCase do
       import Ecto.Changeset
       import Ecto.Query
       # import Bonfire.UI.Messages.DataCase
+
+      @moduletag :backend
     end
   end
 
