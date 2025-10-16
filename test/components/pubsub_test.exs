6bffd9c36e52f62c92a544911d2c3621be0042a5
Mayel de Borniol <git@mayel.space>
2025-10-16 23:12:40 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/1588

--- test/components/pubsub_test.exs
+++ test/components/pubsub_test.exs
@@ -1,5 +1,5 @@
 defmodule Bonfire.UI.Messages.PubSub.Test do
-  use Bonfire.UI.Messages.ConnCase, async: true
+  use Bonfire.UI.Messages.ConnCase, async: System.get_env("TEST_UI_ASYNC") != "no"
   @moduletag :ui
   alias Bonfire.Social.Fake
   alias Bonfire.Social.Boosts
--- test/smart_input/create_message_test.exs
+++ test/smart_input/create_message_test.exs
@@ -1,5 +1,5 @@
 defmodule Bonfire.UI.Messages.CreateMessageTest do
-  use Bonfire.UI.Messages.ConnCase, async: true
+  use Bonfire.UI.Messages.ConnCase, async: System.get_env("TEST_UI_ASYNC") != "no"
   use Bonfire.Common.Utils
   import Bonfire.Files.Simulation
 
